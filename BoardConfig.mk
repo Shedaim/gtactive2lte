b26c6a328baa3935f5f1cf77b9a43aedf54c3aa5
shedaim <ronibeitel@gmail.com>
2018-07-26 11:56:37 +0300

    disabled dm-verity, added support for SuperSu

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -68,6 +68,15 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery.fstab
 
 ############# TWRP specific build flags ##################
 TW_THEME := portrait_mdpi
+<<<<<<< HEAD
+
+# Download mode for Samsung devices
+TW_HAS_DOWNLOAD_MODE := true
+
+# remove button to reboot to bootloader as Samsung devices use Download mode
+TW_NO_REBOOT_BOOTLOADER := true
+
+=======
 # RECOVERY_SDCARD_ON_DATA := true
 # Download mode for Samsung devices
 TW_HAS_DOWNLOAD_MODE := true
@@ -75,6 +84,7 @@ TW_HAS_DOWNLOAD_MODE := true
 TW_NO_REBOOT_BOOTLOADER := true
 
 
+>>>>>>> d666b1d72aecf6ffffd81b93d366adf57e7c16df
 # No MTP in recovery. Allows easy use of adb automatically
 TW_EXCLUDE_MTP := true
 
