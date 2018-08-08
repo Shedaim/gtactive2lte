cb56bdbbb0f362e099172d8d0ec1555f79b7874a
shedaim <ronibeitel@gmail.com>
2018-08-08 14:45:20 +0300

    Moved kernel to separated repo

--- kernel/drivers/video/fbdev/exynos/decon_7870/decon_notify.h
+++
@@ -1,6 +0,0 @@
-#include <linux/notifier.h>
-
-int decon_register_notifier(struct notifier_block *nb);
-int decon_unregister_notifier(struct notifier_block *nb);
-int decon_notifier_call_chain(unsigned long val, void *v);
-
